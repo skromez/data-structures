61ff3bac97bb7cc56b020f821097f2b3364c44b4
Dmitry <ddnovikov@vtb.ru>
2021-05-23 21:54:28 +0300

    test

--- stack/index.ts
+++ stack/index.ts
@@ -1,1 +1,29 @@
-import { DoublyLinkedList } from '../doubly-linked-list';export class Stack<T> {  private list = new DoublyLinkedList<T>()  stack: T[] = []  public isEmpty(): boolean {    return this.size() === 0;  }  public size(): number {    return this.list.getSize()  }  public push(elem: T) {    this.list.addLast(elem)  }  public pop(): T {    if (this.isEmpty()) throw new Error('empty stack')    return this.list.removeLast();  }  public peek(): T {    if (this.isEmpty()) throw new Error('empty stack')    return this.list.peekLast();  }}
+import { DoublyLinkedList } from '../doubly-linked-list';
+
+export class Stack<T> {
+  private list = new DoublyLinkedList<T>()
+  stack: T[] = []
+
+  public isEmpty(): boolean {
+    return this.size() === 0;
+  }
+
+  public size(): number {
+    return this.list.getSize()
+  }
+
+  public push(elem: T) {
+    this.list.addLast(elem)
+  }
+
+  public pop(): T {
+    if (this.isEmpty()) throw new Error('empty stack')
+    return this.list.removeLast();
+  }
+
+  public peek(): T {
+    if (this.isEmpty()) throw new Error('empty stack')
+    return this.list.peekLast();
+  }
+}
+
